cba2933c5e76c9faa385c42c31aa0e92eecbeda3
sdara <seth.dara@gmail.com>
2017-08-19 22:46:52 -0400

    Basic Functionality

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+$( document ).ready( function() {
+	$( 'input[type=button]' ).click( function() {
+		var searchType = $( this ).attr( 'data-searchType' ) || '';
+		var searchArea = $.trim( $( '#searchArea' ).val() );
+		if( searchType !== '' && searchArea !== '' ) {
+			$.ajax( {
+				url: 'http://api.openweathermap.org/data/2.5/' + searchType + '?q=' + searchArea + '&cnt=16&units=imperial&appid=' + CONFIG.APIKEY,
+				cache: false,
+				dataType: 'json',
+				success: function( result ) {
+					var i, l, j, weatherNow, str = '';
+					
+					if( searchType === 'weather' ) {
+						if( result.main ) {
+							for( i in result.main ) {
+								str += '<div>' + i + ' => ' + result.main[ i ] + '</div>';
+							}
+						}
+					} else if( searchType === 'forecast' ) {
+						if( result.list ) {
+							for( j = 0, l = result.list.length; j < l; j += 1 ) {
+								if( result.list[ j ].main ) {
+									weatherNow = result.list[ j ].weather[ 0 ];
+									str += '<hr />' + result.list[ j ].dt_txt + ' : ' + weatherNow.main + ' - ' + weatherNow.description;
+									for( i in result.list[ j ].main ) {
+										str += '<div>' + i + ' => ' + result.list[ j ].main[ i ] + '</div>';
+									}
+								}
+							}
+						}
+					}
+					
+					$( '#output' ).empty().html( str );
+				}
+			} );
+		}
+	} );
+} );
