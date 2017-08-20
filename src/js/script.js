d3fed7ce9cb25494d6a29d3ab9a58de6d97f2f0c
sdara <seth.dara@gmail.com>
2017-08-19 23:12:51 -0400

    Add some comparison checks

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,34 @@ $( document ).ready( function() {
 	$( 'input[type=button]' ).click( function() {
 		var searchType = $( this ).attr( 'data-searchType' ) || '';
 		var searchArea = $.trim( $( '#searchArea' ).val() );
+		
+		var tempMin = $( '#tempMin' ).val();
+		var tempMax = $( '#tempMax' ).val();
+		var humidityMin = $( '#humidityMin' ).val();
+		var humidityMax = $( '#humidityMax' ).val();
+		
+		
 		if( searchType !== '' && searchArea !== '' ) {
 			$.ajax( {
-				url: 'http://api.openweathermap.org/data/2.5/' + searchType + '?q=' + searchArea + '&cnt=16&units=imperial&appid=' + CONFIG.APIKEY,
+				url: 'http://api.openweathermap.org/data/2.5/' + searchType + '?q=' + searchArea + '&cnt=64&units=imperial&appid=' + CONFIG.APIKEY,
 				cache: false,
 				dataType: 'json',
 				success: function( result ) {
-					var i, l, j, weatherNow, str = '';
+					var i, l, j, extra, val, weatherNow, str = '';
 					
 					if( searchType === 'weather' ) {
 						if( result.main ) {
 							for( i in result.main ) {
-								str += '<div>' + i + ' => ' + result.main[ i ] + '</div>';
+								extra = '';
+								val = parseInt( result.main[ i ] );
+								if( 
+									( i === 'temp' && val >= tempMin && val <= tempMax )
+									|| ( i === 'humidity' && val >= humidityMin && val <= humidityMax ) 
+								) {
+									extra = '<span class="green bold">MATCH</span>';
+								}
+								
+								str += '<div>' + i + ' => ' + val + ' ' + extra + '</div>';
 							}
 						}
 					} else if( searchType === 'forecast' ) {
@@ -25,7 +41,16 @@ $( document ).ready( function() {
 									weatherNow = result.list[ j ].weather[ 0 ];
 									str += '<hr />' + result.list[ j ].dt_txt + ' : ' + weatherNow.main + ' - ' + weatherNow.description;
 									for( i in result.list[ j ].main ) {
-										str += '<div>' + i + ' => ' + result.list[ j ].main[ i ] + '</div>';
+										extra = '';
+										val = parseInt( result.list[ j ].main[ i ] );
+										if( 
+											( i === 'temp' && val >= tempMin && val <= tempMax )
+											|| ( i === 'humidity' && val >= humidityMin && val <= humidityMax ) 
+										) {
+											extra = '<span class="green bold">MATCH</span>';
+										}
+										
+										str += '<div>' + i + ' => ' + val + ' ' + extra + '</div>';
 									}
 								}
 							}
